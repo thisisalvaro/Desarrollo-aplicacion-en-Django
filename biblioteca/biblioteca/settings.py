b918fc2cac62ec1101262bb0c5c1b4372652e666
thisisalvaro <alvaroblanco496@gmail.com>
2025-01-30 16:50:38 +0100

    (improve/fixed) new css file and venv install

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' #aqui donde estan a nivel de ruta osea que enlazamos con la carpeta statics mediante esa ruta
+STATICFILES_DIRS = [   #aqui donde estan a nivel de archivo 
+    BASE_DIR / "statics",   
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
